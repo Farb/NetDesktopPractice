af49946f913d179124eb2b08e7f45ef231451bac
farb <farbguo@qq.com>
2020-12-11 01:07:36 +0800

    添加项目文件。

---
+++ WinFormSharpMapDemo/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormSharpMapDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SharpMap.Layers.VectorLayer vlay = new SharpMap.Layers.VectorLayer("States");
+            vlay.DataSource = new SharpMap.Data.Providers.ShapeFile("path_to_data\\states_ugl.shp", true);
+            mapBox1.Map.Layers.Add(vlay);
+            mapBox1.Map.ZoomToExtents();
+            mapBox1.Refresh();
+        }
+    }
+}
