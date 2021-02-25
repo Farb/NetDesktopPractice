c3b32432c9d24b37be1a64246ad6bcf3591bbec6
farb <farbguo@qq.com>
2021-02-25 23:55:36 +0800

    wpf mvvm 小试牛刀

---
+++ WpfMvvmDemo/Commands/DelegateCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfMvvmDemo.Commands
+{
+    class DelegateCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteFunction?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            ExecuteAction(parameter);
+        }
+
+        public Action<object> ExecuteAction { get; set; }
+
+        public Func<object, bool> CanExecuteFunction { get; set; }
+    }
+}
---
+++ WpfMvvmDemo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfMvvmDemo.Commands;
+
+namespace WpfMvvmDemo.ViewModels
+{
+    class MainWindowViewModel : NotificationObject
+    {
+        public MainWindowViewModel()
+        {
+            AddCommand.ExecuteAction = Add;
+            SaveCommand.ExecuteAction = Save;
+        }
+        private double input1;
+
+        public double Input1
+        {
+            get { return input1; }
+            set
+            {
+                input1 = value;
+                RaisePropertyChanged(nameof(Input1));
+            }
+        }
+
+        private double input2;
+
+        public double Input2
+        {
+            get { return input2; }
+            set { input2 = value; RaisePropertyChanged(nameof(Input2)); }
+        }
+
+        private double result;
+
+        public double Result
+        {
+            get { return result; }
+            set { result = value;); RaisePropertyChanged(nameof(Result)); }
+        }
+
+        public void Add(object parameter)
+        {
+            Result = Input1 + Input2;
+        }
+
+        public void Save(object o)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.ShowDialog();
+        }
+        public DelegateCommand AddCommand { get; set; } = new DelegateCommand();
+        public DelegateCommand SaveCommand { get; set; } = new DelegateCommand();
+
+
+    }
+}
