c0d4757741ce36d6aa8494715adc3508ce37f480
farb <farbguo@qq.com>
2021-02-28 00:15:32 +0800

    xaml 详细学习

---
+++ WpfAppNetTest/Human.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace WpfAppNetTest
+{
+    [TypeConverter(typeof(StringToHumanTypeConverter))]
+    public class Human
+    {
+        public string Name { get; set; }
+
+        public Human Child { get; set; }
+    }
+}
---
+++ WpfAppNetTest/StringToHumanTypeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Text;
+
+namespace WpfAppNetTest
+{
+    public class StringToHumanTypeConverter:TypeConverter
+    {
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value is string)
+            {
+                var human = new Human
+                {
+                    Name = value.ToString()
+                };
+                return human;
+            }
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
+}
