e0feeec1217294fffc29d823bb40ca626a9f8f1a
farb <farbguo@qq.com>
2021-03-23 00:13:50 +0800

    DataTable 和XML 绑定

---
+++ WpfAppNetTest/Views/BindingDemo4DataTable.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfAppNetTest.Model;
+
+namespace WpfAppNetTest.Views
+{
+    /// <summary>
+    /// BindingDemo04DataTable.xaml 的交互逻辑
+    /// </summary>
+    public partial class BindingDemo4DataTable : Window
+    {
+        public BindingDemo4DataTable()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //获取DataTable实例
+            DataTable dataTable = Load();
+            //v1
+            //listBoxStudents.DisplayMemberPath = "Name";
+            //listBoxStudents.ItemsSource = dataTable.DefaultView;
+
+            //v2
+            //listViewStudents.ItemsSource = dataTable.DefaultView;
+
+            //v3
+            listViewStudents.DataContext = dataTable;
+            listViewStudents.SetBinding(ListView.ItemsSourceProperty, new Binding());
+        }
+
+        private DataTable Load()
+        {
+            var students = new List<StudentVm>
+            {
+                new StudentVm{Id=1,Name="aaa",Age=30},
+                new StudentVm{Id=2,Name="eee",Age=29},
+                new StudentVm{Id=3,Name="bbb",Age=28},
+                new StudentVm{Id=4,Name="ddd",Age=27},
+                new StudentVm{Id=5,Name="ccc",Age=26},
+            };
+            var dataTable = new DataTable("studentsTable");
+            dataTable.Columns.Add("Id", typeof(int));
+            dataTable.Columns.Add("Name", typeof(string));
+            dataTable.Columns.Add("Age", typeof(int));
+            foreach (var student in students)
+            {
+                DataRow row = dataTable.NewRow();
+                row.SetField("Id",student.Id);
+                row.SetField("Name",student.Name);
+                row.SetField("Age",student.Age);
+                dataTable.Rows.Add(row);
+            }
+            return dataTable;
+        }
+    }
+}
---
+++ WpfAppNetTest/Views/BindingDemo5Xml.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Xml;
+
+namespace WpfAppNetTest.Views
+{
+    /// <summary>
+    /// BindingDemo5Xml.xaml 的交互逻辑
+    /// </summary>
+    public partial class BindingDemo5Xml : Window
+    {
+        public BindingDemo5Xml()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var doc = new XmlDocument();
+            var dataPath = Path.Combine(Directory.GetCurrentDirectory(),"Data/Students.xml");
+            doc.Load(dataPath);
+
+            var xdp = new XmlDataProvider();
+            xdp.Document = doc;
+
+            //v2
+            //var xdp = new XmlDataProvider();
+            //xdp.Source = new Uri(dataPath);
+            //使用XPath选择需要暴露的数据
+            xdp.XPath = "/Students/Student";
+            listViewStudents.DataContext = xdp;
+            listViewStudents.SetBinding(ListView.ItemsSourceProperty, new Binding());
+        }
+    }
+}
