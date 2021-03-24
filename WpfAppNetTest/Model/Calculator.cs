96dae9d0f067dd187e15ffc452366cf7a174e68b
farb <farbguo@qq.com>
2021-03-25 00:09:56 +0800

    Binding for ObjectDataProvider

---
+++ WpfAppNetTest/Model/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace WpfAppNetTest.Model
+{
+    class Calculator
+    {
+        public string Add(string arg1,string arg2)
+        {
+            if (double.TryParse(arg1,out double x)&&double.TryParse(arg2,out double y))
+            {
+                return (x + y).ToString();
+            }
+            return "Input error!";
+        }
+    }
+}
---
+++ WpfAppNetTest/Views/BindingDemo6ObjectDataProvider.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfAppNetTest.Model;
+
+namespace WpfAppNetTest.Views
+{
+    /// <summary>
+    /// ObjectDataProviderDemo.xaml 的交互逻辑
+    /// </summary>
+    public partial class ObjectDataProviderDemo : Window
+    {
+        public ObjectDataProviderDemo()
+        {
+            InitializeComponent();
+            SetBinding();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var odp = new ObjectDataProvider
+            {
+                ObjectInstance = new Calculator(),
+                MethodName = "Add",
+            };
+            odp.MethodParameters.Add("1");
+            odp.MethodParameters.Add("2");
+            MessageBox.Show(odp.Data.ToString());
+        }
+
+        private void SetBinding()
+        {
+            //创建并配置ObjectDataProvider对象
+            var odp = new ObjectDataProvider();
+            odp.ObjectInstance = new Calculator();
+            odp.MethodName = "Add";
+            odp.MethodParameters.Add("0");
+            odp.MethodParameters.Add("0");
+
+            //以ObjectDataProvider对象为Source创建Binding
+            var bindingArg1 = new Binding("MethodParameters[0]")
+            {
+                Source = odp,
+                BindsDirectlyToSource = true,
+                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+            };
+
+            var bindingArg2 = new Binding("MethodParameters[1]")
+            {
+                Source = odp,
+                BindsDirectlyToSource = true,
+                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+            };
+
+            var bindingToResult = new Binding(".") { Source = odp };
+
+            txtArg1.SetBinding(TextBox.TextProperty, bindingArg1);
+            txtArg2.SetBinding(TextBox.TextProperty, bindingArg2);
+            txtResult.SetBinding(TextBox.TextProperty, bindingToResult);
+        }
+    }
+}
