730211944e3313104bf1701525506cb1f5d398ed
farb <farbguo@qq.com>
2021-03-26 00:22:39 +0800

    Binding for Validation

---
+++ WpfAppNetTest/Validations/RangeValidationRule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+
+namespace WpfAppNetTest.Validations
+{
+    public class RangeValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (double.TryParse(value.ToString(),out double dValue))
+            {
+                if (0 <= dValue && dValue <= 100)
+                    return new ValidationResult(true, null);
+            }
+            return new ValidationResult(false, "Validation failed.");
+        }
+    }
+}
---
+++ WpfAppNetTest/Views/BindingForValidation.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfAppNetTest.Validations;
+
+namespace WpfAppNetTest.Views
+{
+    /// <summary>
+    /// BindingForValidation.xaml 的交互逻辑
+    /// </summary>
+    public partial class BindingForValidation : Window
+    {
+        public BindingForValidation()
+        {
+            InitializeComponent();
+            var binding = new Binding("Value") { Source = slider1 };
+            binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            binding.ValidationRules.Add(new RangeValidationRule() { ValidatesOnTargetUpdated = true });
+            binding.NotifyOnValidationError = true;
+            txtV.SetBinding(TextBox.TextProperty,binding);
+
+            txtV.AddHandler(Validation.ErrorEvent,new RoutedEventHandler(ValidationErrorHandle));
+        }
+
+        private void ValidationErrorHandle(object sender, RoutedEventArgs e)
+        {
+            if (Validation.GetHasError(txtV))
+            {
+                txtV.ToolTip = Validation.GetErrors(txtV)[0].ErrorContent.ToString();
+            }
+        }
+    }
+}
