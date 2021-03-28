b37e5ca7befa1a1423ad53ed8d977698750af38a
farb <farbguo@qq.com>
2021-03-28 23:24:52 +0800

    Dependency Property存储与访问秘密

--- WpfAppNetTest/Model/Student.cs
+++ WpfAppNetTest/Model/Student.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using System.Windows;
+using System.Windows.Data;
 
 namespace WpfAppNetTest.Model
 {
-    public class Student:INotifyPropertyChanged
+    public class Student:DependencyObject,INotifyPropertyChanged
     {
+        public static readonly DependencyProperty IdProperty =
+            DependencyProperty.Register("Id",typeof(string),typeof(Student));
         private string name;
 
         public string Name
@@ -15,6 +19,24 @@ namespace WpfAppNetTest.Model
             set { name = value; RaisePropertyChanged(nameof(Name)); }
         }
 
+        public string Id { get=>GetValue(IdProperty).ToString(); set=>SetValue(IdProperty,value); }
+
+
+        public int No
+        {
+            get { return (int)GetValue(NoProperty); }
+            set { SetValue(NoProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for No.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NoProperty =
+            DependencyProperty.Register("No", typeof(int), typeof(Student), new PropertyMetadata(0));
+
+
+        public BindingExpressionBase SetBinding(DependencyProperty dp,BindingBase binding)
+        {
+            return BindingOperations.SetBinding(this, IdProperty, binding);
+        }
         private void RaisePropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
--- WpfAppNetTest/Views/DependencyProperty/DpDemo1.xaml.cs
+++ WpfAppNetTest/Views/DependencyProperty/DpDemo1.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WpfAppNetTest.Model;
 
 namespace WpfAppNetTest.Views.DependencyProperty
 {
@@ -17,14 +18,36 @@ namespace WpfAppNetTest.Views.DependencyProperty
     /// </summary>
     public partial class DpDemo1 : Window
     {
+        Student stu;
         public DpDemo1()
         {
             InitializeComponent();
+            stu = new Student();
+            //v1
+            //var binding = new Binding("Text") { Source = txt1 };
+            //BindingOperations.SetBinding(stu, Student.IdProperty, binding);
+            //txt2.SetBinding(TextBox.TextProperty, binding);
+
+            //v2
+            stu.SetBinding(Student.IdProperty, new Binding("Text") { Source = txt1 });
+            txt2.SetBinding(TextBox.TextProperty, new Binding("Id") { Source = stu });
         }
 
         private void btnOk_Click(object sender, RoutedEventArgs e)
         {
+            var student = new Student();
+            //v1
+            //student.SetValue(Student.IdProperty, txt1.Text);
+            //txt2.Text = (string)student.GetValue(Student.IdProperty);
 
+            //v2
+            student.Id = txt1.Text;
+            txt2.Text = student.Id;
+        }
+
+        private void btnO2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(stu.GetValue(Student.IdProperty).ToString());
         }
     }
 }
