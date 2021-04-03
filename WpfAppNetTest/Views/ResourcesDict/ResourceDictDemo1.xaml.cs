57e28139414ee73533055c585136405179bae4b1
farb <farbguo@qq.com>
2021-04-04 00:11:30 +0800

    资源

---
+++ WpfAppNetTest/Views/ResourcesDict/ResourceDictDemo1.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfAppNetTest.Views.ResourcesDict
+{
+    /// <summary>
+    /// ResourceDictDemo1.xaml 的交互逻辑
+    /// </summary>
+    public partial class ResourceDictDemo1 : Window
+    {
+        public ResourceDictDemo1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var value = TryFindResource("str")?.ToString();
+            tb3.Text = value;
+
+            txtFromResource.Text = Resource1.testResourceFile;
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            Resources["txt1"] = new TextBlock { Text = "天涯共此时" };
+            Resources["txt2"] = new TextBlock { Text = "天涯共此时" };
+        }
+
+    }
+}
