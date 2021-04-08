0b6c21994561535157dbfdc54a0cc0d224e5febc
farb <farbguo@qq.com>
2021-04-09 00:30:14 +0800

    Style 练习

---
+++ WpfAppNetTest/Converter/Length2BoolConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfAppNetTest.Converter
+{
+    class Length2BoolConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string str = value.ToString();
+            return str.Length > 6;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfAppNetTest/Views/Styles/TriggerDemo2.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfAppNetTest.Model;
+
+namespace WpfAppNetTest.Views.Styles
+{
+    /// <summary>
+    /// TriggerDemo2.xaml 的交互逻辑
+    /// </summary>
+    public partial class TriggerDemo2 : Window
+    {
+        public TriggerDemo2()
+        {
+            InitializeComponent();
+            InitializeData();
+        }
+
+        private void InitializeData()
+        {
+            var stuList = new List<StudentVm>
+            {
+                new StudentVm{Id=1,Name="Alice",Age=18},
+                new StudentVm{Id=3,Name="Bob",Age=19},
+                new StudentVm{Id=2,Name="Jack",Age=20},
+                new StudentVm{Id=4,Name="Jenny",Age=17},
+            };
+
+            listBoxStudent.ItemsSource = stuList;
+        }
+    }
+}
