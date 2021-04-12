79c0ee71af0b563d265affee979c287b1817e90d
farb <farbguo@qq.com>
2021-04-12 22:43:52 +0800

    Effects和Transform

---
+++ WpfAppNetTest/Views/Drawings/EffectDemo.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfAppNetTest.Views.Drawings
+{
+    /// <summary>
+    /// Effect.xaml 的交互逻辑
+    /// </summary>
+    public partial class EffectDemo : Window
+    {
+        public EffectDemo()
+        {
+            InitializeComponent();
+        }
+    }
+}
